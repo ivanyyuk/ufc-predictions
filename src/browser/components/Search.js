967d5176df23814eb9ef85820faff16fcf5b76ab
yuriy ivanyuk <yuriy@yuriys-MBP.home>
2017-03-20 15:05:35 -0400

    small change before removing create-react-app

--- src/browser/components/Search.js
+++ src/browser/components/Search.js
@@ -4,8 +4,8 @@ export default ({handleChange, handleSubmit}) => {
   return(
     <div>
       <form onSubmit={handleChange}>
-      <input type='text' placeHolder='Fighter 1' onChange={handleChange} />
-      <input type='text' placeHolder='Fighter 2' onChange={handleChange} />
+      <input type='text' placeholder='Fighter 1' onChange={handleChange} />
+      <input type='text' placeholder='Fighter 2' onChange={handleChange} />
       <button type='submit'>Show me the winner </button>
     </form>
     </div>
---
+++ src/browser/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Routes from './browser/routes.js';
+import './browser/index.css';
+
+ReactDOM.render(
+  <Routes />,
+  document.getElementById('root')
+);
