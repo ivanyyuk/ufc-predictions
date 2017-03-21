b7f65728c5e3f3c3be819113acba24fcf3988947
yuriy ivanyuk <yuriy@yuriys-MBP.home>
2017-03-21 18:26:28 -0400

    search page

--- src/browser/components/Search.js
+++ src/browser/components/Search.js
@@ -2,12 +2,23 @@ import React from 'react';
 
 export default ({handleChange, handleSubmit}) => {
   return(
-    <div>
-      <form onSubmit={handleChange}>
-      <input type='text' placeholder='Fighter 1' onChange={handleChange} />
-      <input type='text' placeholder='Fighter 2' onChange={handleChange} />
-      <button type='submit'>Show me the winner </button>
-    </form>
+    <div className='tc'>
+      <form onSubmit={handleSubmit}>
+        <input type='text'
+          className='input-reset center ba b--black-20 pa2 mb2 db w-90'
+          placeholder='Fighter 1' onChange={handleChange} 
+        />
+        <input type='text' 
+          className='input-reset center ba b--black-20 pa2 mb2 db w-90'
+          placeholder='Fighter 2' onChange={handleChange} 
+        />
+
+        <input
+          type="submit"
+          className="f6 link br-pill ba bw1 ph3 pv2 dib navy"
+          value="Predict"
+        />
+      </form>
     </div>
   );
 };
