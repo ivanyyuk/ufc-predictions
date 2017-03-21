5a0178e90b5b2a6904ee920e9b5060a2c64d6060
yuriy ivanyuk <yuriy@yuriys-MBP.home>
2017-03-21 17:24:50 -0400

    more design stuff

--- src/browser/components/EventBox.js
+++ src/browser/components/EventBox.js
@@ -2,14 +2,16 @@ import React from 'react';
 import {Link} from 'react-router';
 
 const EventBox = ({name, tagLine, image, date, id}) => ( 
-  <Link to={`/events/${id}`}>
-    <div>
-      <h1>{name}</h1>
-      <br/>
-      <h2>{tagLine}</h2>
-      <img src={image} alt={name} />
-      <p>{date}</p>
-    </div>
+  <Link className='link black' to={`/events/${id}`}>
+    <article className="center bg-white br3 pa3 pa4-ns mv3 ba b--black-10">
+      <div className="tc">
+        <h1 className="f3 mb2">{name}</h1>
+        <img src={image} alt={name}
+          className=" dib ba b--black-05 pa2" />
+        <h2 className="f4 fw4 gray mt0">{tagLine}</h2>
+        <h3 className="f5 fw4 gray mt0">{date}</h3>
+      </div>
+    </article>
   </Link>
 );
 
