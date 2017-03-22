461764a42533c0d9fa52743dbf1ec18d6063cd18
yuriy ivanyuk <yuriy@yuriys-MBP.home>
2017-03-22 16:53:35 -0400

    server side search works but greedy

--- src/server/api/search.js
+++ src/server/api/search.js
@@ -1,28 +1,30 @@
 'use strict';
 
 const router = require('express').Router();
-const Fighters = require('../db/models/fighters');
+const Fighter = require('../db/models/fighters');
+const Bluebird = require('bluebird');
+const _ = require('lodash');
 
 router.post('/', (req, res, next) => {
   const searchText = req.body.searchText
     .split(' ')
-    .map(elem => '(?=.*' + elem + ')')
-    .join('');
-  
-  const searchExp = new RegExp(searchText, 'ig');
+    .map(elem => '(?=.*' + elem + ')');
+
+
+  const searchExp = searchText.map(exp => new RegExp(exp, 'ig'));
   const searches = [];
 
-  console.log(searchText);
-
-  return Fighters.find({
-    $or: [
-      { 'name.first': searchExp },
-      { 'name.last': searchExp },
-      { 'name.nick': searchExp }
-    ]
+  Bluebird.each(searchExp, function (exp) {
+   return Fighter.find({
+      $or: [
+        { first_name: exp }, { last_name: exp}]
+    })
+    .then(found => searches.push(found))
   })
-    .then(foundFighters => res.send(foundFighters))
-    .catch(next);
+  .then(() => _.flatten(searches))
+  .then(flatSearch => res.json(_.uniqBy(flatSearch, 'id')))
+  .catch(next);
+
 });
 
 module.exports = router;
