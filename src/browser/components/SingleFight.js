4f21093efba5d42d77d72322ac8a8826ffcaac2b
yuriy ivanyuk <yuriy@yuriys-MBP.home>
2017-03-27 01:48:39 -0400

    fixed small styling bug

--- src/browser/components/SingleFight.js
+++ src/browser/components/SingleFight.js
@@ -6,7 +6,7 @@ export default ({ currentFight }) => {
   const record2 = `${currentFight.fighter2.record.wins.total}-${currentFight.fighter2.record.losses.total}-${currentFight.fighter2.record.draws.total}`;
   return ( 
         <div className='single-fight flex flex-column center'>
-          <div className='versus-item clearfix border xs-col-8 sm-col-8 md-col-8 mx-auto'>
+          <div className='clearfix border xs-col-8 sm-col-8 md-col-8 mx-auto'>
             <Fighter firstName={currentFight.fighter1.first_name} 
               lastName={currentFight.fighter1.last_name}
               record={record1}
@@ -20,7 +20,7 @@ export default ({ currentFight }) => {
           </div>
 
           <div className=' my2 mx-auto '>
-            <div className='loader col-6'></div><span className='col-6'>Loading ... </span>
+            <div className='loader  col-6'></div><span className='col-6'>Loading ... </span>
         </div>
         </div>
   );
