9d92a7b2bbd7be06b8f532fd65af1d39729588ab
yuriy ivanyuk <yuriy@yuriys-MBP.home>
2017-03-30 00:50:45 -0400

    navbar better

--- src/browser/components/Navbar.js
+++ src/browser/components/Navbar.js
@@ -3,37 +3,51 @@ import React from 'react';
 
 export default () => {
   return(
-    <div className='navbar'>
-    <div className='logo row black center-align'>
-      <div className='col s12'>
-        <Link to='/'>
-          <h1>UFC FIGHT PREDICTIONS</h1>
-        </Link>
+    <div>
+      <div className='logo row black'>
+        <h1 className='center-align'>MY FIGHT PREDICTIONS </h1>
       </div>
+      
+      <nav className='navbar row black'>
+        <div className='nav-wrapper'>
+          <div className='row center-align'>
+            <ul>
+              <li className='col s4'>  <IndexLink to='/'
+                  activeClassName='blue'
+                >
+                  Home
+                </IndexLink> 
+              </li>
 
+              <li className='col s4'> <Link to='/events'
+                  activeClassName='blue'
+                >
+                  <span className='hide-on-small-only'>Upcoming</span> Events
+                </Link>
+              </li>
+
+              <li className='col s4'> <Link to='/search'
+                  activeClassName='blue'
+                >
+                  Search
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
-      <div className='nav-buttons row'>
-
-          <IndexLink to='/'
-            activeClassName='black'
-            className='btn col s4'>
-            Home
-          </IndexLink> 
-
-          <Link to='/events'
-            activeClassName='black'
-            className='col s4 btn'>
-            Upcoming Events
-          </Link>
-
-          <Link to='/search'
-            activeClassName='black'
-            className='col s4 btn'>
-            Search
-          </Link>
-
-      </div>
-      </div>
   );
 };
-
+/*
+     <nav>
+    <div class="nav-wrapper">
+      <a href="#!" class="brand-logo center">Logo</a>
+      <ul class="left hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li class="active"><a href="collapsible.html">JavaScript</a></li>
+      </ul>
+    </div>
+  </nav>
+  */
